a732539d510eef81bc22a50ed02438f262e39de1
AdrianaSche <schepers2008@hotmail.de>
2021-08-05 17:01:20 +0200

    add fetch modul

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import './App.css'
 import response from './characters-response.json'
 import Header from './components/Header'
 import CharacterGallery from './components/CharacterGallery'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { fetchAllCharacters } from './services/fetchAllCharacters'
 
 function App() {
   //setCharacter schreibt in character rein
-  //
   const [characters, setCharacters] = useState([])
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    fetchAllCharacters()
+      .then(characters => setCharacters(characters))
+      .catch(error => console.error(error))
+  }, [])
+
   const loadCharacters = () => {
     setCharacters(response.results)
   }
   const clearCharacters = () => setCharacters([])
 
   const searchHandler = event => setSearch(event.target.value)
-  const filteredCharacters = () =>
-    characters.filter(character =>
-      character.name.toLowerCase().includes(search.toLowerCase())
-    )
+  const filteredCharacters = characters.filter(character =>
+    character.name.toLowerCase().includes(search.toLowerCase())
+  )
 
   const heading = 'Character Gallery'
   // const characters = response.results
--- src/components/CharacterGallery.js
+++ src/components/CharacterGallery.js
@@ -1,18 +1,12 @@
-import CharacterCard from "./CharacterCard";
+import CharacterCard from './CharacterCard'
 import './CharacterGallery.css'
 
-
 export default function CharacterGallery(props) {
-    return (
-        <div className="character-gallery">
-            {
-                props.characters.map(character=>
-                     <CharacterCard key={character.id} character={character}/>
-                )
-
-            }
-
-        </div>
-    );
-
+  return (
+    <div className="character-gallery">
+      {props.characters.map(character => (
+        <CharacterCard key={character.id} character={character} />
+      ))}
+    </div>
+  )
 }
