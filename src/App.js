7b152d9b3d52f07b1052cdaca4d5a5612c504d9c
AdrianaSche <schepers2008@hotmail.de>
2021-08-05 12:52:30 +0200

    Add Usestate to button

--- src/App.js
+++ src/App.js
@@ -3,12 +3,19 @@ import './App.css';
 import Header from "./components/Header";
 import response from './characters-response.json'
 import CharacterGallery from "./components/CharacterGallery";
+import {useState} from "react";
 
 function App() {
+
+  const[characters,setCharacters] = useState([])
+  const loadCharacters = () =>{
+    setCharacters(response.results)
+  }
   const heading = "Character Gallery"
-  const characters = response.results
+ // const characters = response.results
   return <div>
     <Header title={heading}/>
+    <button onClick={loadCharacters}>load cards</button>
     <CharacterGallery characters={characters}/>
   </div>
 
